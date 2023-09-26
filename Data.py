ab222dbbfb87a4e5024c001727846295cf35fdd7
Blake Mountford <bmou8324@usyd.edu.au>
2023-09-26 20:02:26 +1000

    Restructuring, added filetree and class hierachy, cleaned up main.py, created config.json

--- Data.py
+++ Data.py
@@ -1,33 +1,88 @@
 import os.path, json, requests
 from pprint import pprint
 
-def make_request(filepath, url_prefix : str, url_ids : list, headers = {}) -> list:
-    """ If the data exists in a file at filepath, then read that file.
-    If not, then request that data from the url.
-    Take the data from the request, sanitise it and format it.
-    Store that data in a file at filepath.
-    Return the data as a List of JSON Objects. """
+# def make_request(filepath, url_prefix : str, url_ids : list, headers = {}) -> list:
+#     """ If the data exists in a file at filepath, then read that file.
+#     If not, then request that data from the url.
+#     Take the data from the request, sanitise it and format it.
+#     Store that data in a file at filepath.
+#     Return the data as a List of JSON Objects. """
+    
+#     data = []
+
+#     # Check if we already have the requested data stored in filepath
+#     if os.path.isfile(filepath):
+#         """ Load the data from the file
+#         This assumes the file is healthy and accurate
+#         (probably shouldn't be assumed in the final version)"""
+#         with open(filepath, 'r') as f:
+#             print(f"Reading data from file: {filepath}")
+#             data = json.loads(f.read().encode("ascii", "ignore"))
+#     else:
+#         # Make api requests for each url prefix+id pairing
+#         for id in url_ids:
+#             url = url_prefix + str(id)
+#             print(f"Making request for url: {url}")
+#             # Remove non-ascii characters, thanks bird.
+#             resp = requests.get(url, headers=headers)
+#             if resp.status_code !=  200:
+#                 print(f"\tWARNING\tError making request, {resp.status_code}")
+#                 # Should anything else be provided?
+#                 return None # crash the program for now
+#             resp_text = resp.text.encode("ascii", "ignore")
+#             data.append(json.loads(resp_text))
+
+#         # Write these to a file.
+#         with open(filepath, 'w') as f:
+#             f.write(json.dumps(data, indent=4))
+#     return data
+
+def request_batch(cache_filepath_prefix, url_prefix : str, url_ids : list, headers = {}) -> list:
+    """ Make multiple requests for different ID's
+    from the same URL prefix, return data as a List of JSONObjects
+    Store each JSONObject at cache_filepath_prefix/url_id"""
     
     data = []
 
-    # Check if we already have the requested data stored in filepath
-    if os.path.isfile(filepath):
+    # Make sure the cache directory for this batch exists
+    if not os.path.exists(cache_filepath_prefix):
+        os.mkdir(cache_filepath_prefix)
+
+    for id in url_ids:
+        url = url_prefix + str(id)
+        filepath = cache_filepath_prefix + str(id)
+        data.append(request(filepath, url, headers))
+    
+    return data
+
+def request(cache_filepath, url, headers = {}):
+    """ If the data exists in a file in the cache, then read that file.
+    If not, then request that data from the url.
+    Take the data from the request, sanitise it and format it.
+    Store that data in a file in the cache.
+    Return the data as a JSON Object. """
+    data = None
+        # Check if we already have the requested data stored in the cache
+    if os.path.isfile(cache_filepath):
         """ Load the data from the file
         This assumes the file is healthy and accurate
         (probably shouldn't be assumed in the final version)"""
-        with open(filepath, 'r') as f:
-            print(f"Reading data from file: {filepath}")
+        with open(cache_filepath, 'r') as f:
+            print(f"Reading data from cache: {cache_filepath}")
             data = json.loads(f.read().encode("ascii", "ignore"))
+            # TODO: Check if file is healthy, if not, then replace it.
     else:
-        # Make api requests for each url prefix+id pairing
-        for id in url_ids:
-            url = url_prefix + str(id)
-            print(f"Making request for url: {url}")
-            # Remove non-ascii characters, thanks bird.
-            response_text = requests.get(url, headers=headers).text.encode("ascii", "ignore")
-            data.append(json.loads(response_text))
+        # Make API request and store it
+        print(f"Making request for url: {url}")
+        resp = requests.get(url, headers=headers)
+        if resp.status_code !=  200:
+            print(f"\tWARNING\tError making request, {resp.status_code}")
+            return None # Crash the program for now
+        # Remove non-ascii characters, thanks bird.
+        resp_text = resp.text.encode("ascii", "ignore")
+        data = json.loads(resp_text)
 
-        # Write these to a file.
-        with open(filepath, 'w') as f:
+        # Write these to the cache.
+        with open(cache_filepath, 'w') as f:
             f.write(json.dumps(data, indent=4))
     return data
--- README.md
+++ README.md
@@ -1,24 +1,26 @@
 TO DO LIST:
 
-    1) [DONE] Hide Private Key in gitignore
+    1) [x] Hide Private Key in gitignore
         1.1) Add a template file where others can put their private keys if they want to clone the repo
-    2) [DONE] Put it on github.
-    3) Add error handling
-        (Option 1): Shut down the app when a request fails and give an error message
+    2) [x] Put it on github.
+    3) [x] Add error handling
+        (Option 1): Shut down the app when a request fails and give an error message [I went with this one]
         (Option 2): Continue when the error is recieved and handle the missing data (risks hammering the API when I'm getting nothing back but errors)
-    4) Add individual log requesting and storing
-        4.1) Decide on directory system for storing individual logs
+    4) [] Add individual log requesting and storing
+        4.1) [] Decide on directory system for storing individual logs
             (Option 1): A single folder with every log named by their ID.   (nothing wrong with this)
             (Option 2): A logs folder with subfolders for each team, which then contains the logs. (neater)
-        4.2) Add multithreading
-        4.3) Add team alignment checking
+        4.2) [] Add multithreading
+        4.3) [] Add team alignment checking
         
-    5) Add team alignment checking
-    6) Add team matchup checking
-    7) Add test cases (no clue)
-    8) Make a config.json file that specifies all filepaths and API URL prefixes, and the season IDs / names they are searching.
-    9) Put all the locally stored API responses in a single filetree, so they arent just floating around in root.
-    10) Add that filetree to .gitignore, no point other people downloading the logs themselves. The application is supposed to find the logs pertaining to any arbitrary season.
+    5) [] Add team alignment checking
+    6) [] Add team matchup checking
+    7) [] Add test cases (no clue)
+    8) [] Make a config.json file that specifies all filepaths and API URL prefixes, and the season IDs / names they are searching.
+    9) [] Put all the locally stored API responses in a single filetree, so they arent just floating around in root.
+    10) [] Add that filetree to .gitignore, no point other people downloading the logs themselves. The application is supposed to find the logs pertaining to any arbitrary season.
+    11) [] Add checking that the stored files contain valid data, and if not, make the API request and overwrite them.
+    12) [] Restructure datatypes and filetree
 
 NOTES: 
 
@@ -28,11 +30,13 @@ NOTES:
 
 - Mercs? unavoidable confusion I think
 
+- Consider how transfers should be handled? Are they recorded in the ozf API? if 2 core players are transferred out of a roster mid season, then post-transfer logs would not be detected properly
+
 ===IMPORTANT IF YOU WANT TO USE THIS PROGRAM YOURSELF===
 
 You will need your own ozfortress API key. I have no idea how to get one, I just asked core on discord and he gave it to me. 
 
-Create a folder called ozf_api_key.json and put this in it:
+Create a folder called secrets and create a file inside it called ozf_api_key.json and put this in it:
 ```
 {
     "key": <your key as a string>
